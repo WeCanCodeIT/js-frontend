2e8469bef5f54484fcbc32c2411019421e29c508
Don Hamilton III <donhamiltoniii@gmail.com>
2019-10-24 20:45:31 -0400

    Refactor procedural code from index

--- src/index.js
+++ src/index.js
@@ -3,53 +3,14 @@ const {
 } = require("blackjack-dealer-logic");
 const Dom = require("./utils/Dom");
 
-const userChips = document.querySelector(".user-chips");
-const playerAnte = prompt(
-  `
-  How much would you like to bet?
-  Current chip count: ${singleDeckGame.getUserChips()}
-  `
-);
-userChips.textContent = playerAnte;
-singleDeckGame.receiveAnte(playerAnte);
+Dom.setUpButtonEvents(singleDeckGame);
+
+Dom.setInitialAnte(singleDeckGame);
 
 singleDeckGame.deal();
 
-const dealerHand = singleDeckGame.getDealerHand();
-const userHand = singleDeckGame.getUserHand();
+Dom.renderHands([
+  { cards: [singleDeckGame.getDealerHand().getCards()[0]], container: ".dealer" },
+  { cards: singleDeckGame.getUserHand().getCards(), container: ".user" },
+]);
 
-Dom.renderCards([dealerHand.getCards()[1]], document.querySelector(".dealer"));
-Dom.renderCards(userHand.getCards(), document.querySelector(".user"));
-
-const hitButton = document.querySelector(".btn--hit");
-
-hitButton.addEventListener("click", () => {
-  singleDeckGame.hitUser();
-  singleDeckGame.evaluateUser();
-  document.querySelector(".user").innerHTML = "";
-  Dom.renderCards(
-    singleDeckGame.getUserHand().getCards(),
-    document.querySelector(".user")
-  );
-});
-
-const doubleButton = document.querySelector(".btn--double");
-
-doubleButton.addEventListener("click", () => {
-  singleDeckGame.doubleUser();
-  userChips.textContent = singleDeckGame.getAnte();
-  singleDeckGame.evaluateUser();
-  document.querySelector(".user").innerHTML = "";
-  Dom.renderCards(
-    singleDeckGame.getUserHand().getCards(),
-    document.querySelector(".user")
-  );
-
-  const actionsContainer = document.querySelector(".actions");
-  const actionButtons = actionsContainer.querySelectorAll("*");
-  actionButtons.forEach(button => button.setAttribute("disabled", "true"));
-});
-
-Dom.addClickEventToButton(".btn--stand", () => {
-  Dom.standEvent(singleDeckGame);
-});
--- src/utils/Dom.js
+++ src/utils/Dom.js
@@ -1,14 +1,31 @@
 module.exports = {
-  addClickEventToButton(identifier, callback) {
+  addClickEventToButton (identifier, callback) {
     const button = document.querySelector(identifier);
     button.addEventListener("click", callback);
   },
-  disableActionButtons() {
+  disableActionButtons () {
     const actionsContainer = document.querySelector(".actions");
     const actionButtons = actionsContainer.querySelectorAll("button");
     actionButtons.forEach(button => button.setAttribute("disabled", "true"));
   },
-  generateCard(card) {
+  doubleEvent (singleDeckGame) {
+    singleDeckGame.doubleUser();
+    userChips.textContent = singleDeckGame.getAnte();
+    singleDeckGame.evaluateUser();
+    document.querySelector(".user").innerHTML = "";
+    this.renderCards(
+      singleDeckGame.getUserHand().getCards(),
+      document.querySelector(".user")
+    );
+
+    this.disableActionButtons()
+
+    // Evaluate User hand
+    // If User is bust, they lose
+    // if not evaluate dealer hand
+    // then find outcome
+  },
+  generateCard (card) {
     const playingCard = document.createElement("section");
     playingCard.classList.add("playing-card");
 
@@ -29,15 +46,55 @@ module.exports = {
 
     return playingCard;
   },
-  renderCards(cardsArray, containerElement) {
+  hitEvent (singleDeckGame) {
+    singleDeckGame.hitUser();
+    singleDeckGame.evaluateUser();
+    document.querySelector(".user").innerHTML = "";
+    this.renderCards(
+      singleDeckGame.getUserHand().getCards(),
+      document.querySelector(".user")
+    );
+    // Check to see if User is bust
+    // if so User loses
+    // otherwise keep going
+  },
+  renderCards (cardsArray, containerElement) {
     cardsArray.forEach(card => {
       containerElement.append(this.generateCard(card));
     });
   },
-  standEvent(singleDeckGame) {
+  renderHands (handArray) {
+    handArray.forEach(hand => {
+      this.renderCards(hand.cards, document.querySelector(hand.container));
+    });
+  },
+  setUpButtonEvents (singleDeckGame) {
+    this.addClickEventToButton(".btn--hit", () => {
+      this.hitEvent(singleDeckGame);
+    });
+    this.addClickEventToButton(".btn--double", () => {
+      this.doubleEvent(singleDeckGame);
+    });
+    this.addClickEventToButton(".btn--stand", () => {
+      this.standEvent(singleDeckGame);
+    });
+  },
+  setInitialAnte (singleDeckGame) {
+    const userChips = document.querySelector(".user-chips");
+    const playerAnte = prompt(`
+    How much would you like to bet?
+    Current chip count: ${singleDeckGame.getUserChips()}
+    `);
+    userChips.textContent = playerAnte;
+    singleDeckGame.receiveAnte(playerAnte);
+  },
+  standEvent (singleDeckGame) {
     singleDeckGame.standUser();
     singleDeckGame.evaluateUser();
 
     this.disableActionButtons();
+
+    // evaluate Dealer hand
+    // then find outcome
   }
 };
