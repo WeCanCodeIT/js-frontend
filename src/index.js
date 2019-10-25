839040309b10a01246f334d31eda1fcdb4415312
Don Hamilton III <donhamiltoniii@gmail.com>
2019-10-25 12:01:36 -0400

    Add outcome logic

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 const {
-  default: { singleDeckGame }
+  default: { singleDeckGame, Result }
 } = require("blackjack-dealer-logic");
 const Dom = require("./utils/Dom");
 
-Dom.setUpButtonEvents(singleDeckGame);
+Dom.setUpButtonEvents(singleDeckGame, Result);
 
 Dom.setInitialAnte(singleDeckGame);
 
 singleDeckGame.deal();
 
 Dom.renderHands([
-  { cards: [singleDeckGame.getDealerHand().getCards()[0]], container: ".dealer" },
-  { cards: singleDeckGame.getUserHand().getCards(), container: ".user" },
+  {
+    cards: [singleDeckGame.getDealerHand().getCards()[0]],
+    container: ".dealer"
+  },
+  { cards: singleDeckGame.getUserHand().getCards(), container: ".user" }
 ]);
-
--- src/utils/Dom.js
+++ src/utils/Dom.js
@@ -1,15 +1,23 @@
 module.exports = {
-  addClickEventToButton (identifier, callback) {
+  addClickEventToButton(identifier, callback) {
     const button = document.querySelector(identifier);
     button.addEventListener("click", callback);
   },
-  disableActionButtons () {
+  addRestartButtonEventListener() {
+    document.body.addEventListener("click", event => {
+      if (event.target.classList.contains("play-again")) {
+        // TODO finish current hand and start a new one
+      }
+    });
+  },
+  disableActionButtons() {
     const actionsContainer = document.querySelector(".actions");
     const actionButtons = actionsContainer.querySelectorAll("button");
     actionButtons.forEach(button => button.setAttribute("disabled", "true"));
   },
-  doubleEvent (singleDeckGame) {
+  doubleEvent(singleDeckGame, Result) {
     singleDeckGame.doubleUser();
+    const userChips = document.querySelector(".user-chips");
     userChips.textContent = singleDeckGame.getAnte();
     singleDeckGame.evaluateUser();
     document.querySelector(".user").innerHTML = "";
@@ -18,14 +26,16 @@ module.exports = {
       document.querySelector(".user")
     );
 
-    this.disableActionButtons()
+    this.disableActionButtons();
 
     // Evaluate User hand
     // If User is bust, they lose
     // if not evaluate dealer hand
     // then find outcome
+
+    this.standEvent(singleDeckGame, Result);
   },
-  generateCard (card) {
+  generateCard(card) {
     const playingCard = document.createElement("section");
     playingCard.classList.add("playing-card");
 
@@ -46,7 +56,7 @@ module.exports = {
 
     return playingCard;
   },
-  hitEvent (singleDeckGame) {
+  hitEvent(singleDeckGame, Result) {
     singleDeckGame.hitUser();
     singleDeckGame.evaluateUser();
     document.querySelector(".user").innerHTML = "";
@@ -54,32 +64,43 @@ module.exports = {
       singleDeckGame.getUserHand().getCards(),
       document.querySelector(".user")
     );
+
     // Check to see if User is bust
     // if so User loses
     // otherwise keep going
+    if (singleDeckGame.isUserBust()) {
+      this.standEvent(singleDeckGame, Result);
+    }
   },
-  renderCards (cardsArray, containerElement) {
+  renderCards(cardsArray, containerElement) {
     cardsArray.forEach(card => {
       containerElement.append(this.generateCard(card));
     });
   },
-  renderHands (handArray) {
+  renderHands(handArray) {
     handArray.forEach(hand => {
       this.renderCards(hand.cards, document.querySelector(hand.container));
     });
   },
-  setUpButtonEvents (singleDeckGame) {
+  renderRestartButton() {
+    const restartButton = document.createElement("button");
+    restartButton.classList.add("play-again");
+    restartButton.textContent = "Play Again!";
+
+    return restartButton;
+  },
+  setUpButtonEvents(singleDeckGame, Result) {
     this.addClickEventToButton(".btn--hit", () => {
-      this.hitEvent(singleDeckGame);
+      this.hitEvent(singleDeckGame, Result);
     });
     this.addClickEventToButton(".btn--double", () => {
-      this.doubleEvent(singleDeckGame);
+      this.doubleEvent(singleDeckGame, Result);
     });
     this.addClickEventToButton(".btn--stand", () => {
-      this.standEvent(singleDeckGame);
+      this.standEvent(singleDeckGame, Result);
     });
   },
-  setInitialAnte (singleDeckGame) {
+  setInitialAnte(singleDeckGame) {
     const userChips = document.querySelector(".user-chips");
     const playerAnte = prompt(`
     How much would you like to bet?
@@ -87,14 +108,51 @@ module.exports = {
     `);
     userChips.textContent = playerAnte;
     singleDeckGame.receiveAnte(playerAnte);
+
+    const chipCountContainer = document.querySelector(".chip-count");
+    chipCountContainer.textContent = singleDeckGame.getUserChips();
   },
-  standEvent (singleDeckGame) {
+  standEvent(singleDeckGame, Result) {
     singleDeckGame.standUser();
     singleDeckGame.evaluateUser();
 
     this.disableActionButtons();
 
     // evaluate Dealer hand
+    singleDeckGame.settleDealerHand();
+
+    const dealerContainer = document.querySelector(".dealer");
+    dealerContainer.innerHTML = "";
+
+    this.renderCards(
+      singleDeckGame.getDealerHand().getCards(),
+      dealerContainer
+    );
+
+    singleDeckGame.evaluateDealer();
+
     // then find outcome
+    const resultContainer = document.querySelector(".result");
+
+    const restartButton = this.renderRestartButton();
+
+    resultContainer.append(restartButton);
+
+    this.addRestartButtonEventListener();
+
+    switch (singleDeckGame.outcome()) {
+      case Result.WIN:
+        resultContainer.innerHTML += " You won!!!";
+        break;
+      case Result.PUSH:
+        resultContainer.innerHTML += " You pushed. Not so bad.";
+        break;
+      case Result.LOSS:
+        resultContainer.innerHTML += " You lost!!!";
+        break;
+
+      default:
+        break;
+    }
   }
 };
